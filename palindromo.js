922b1535c67c514df2781910c9f656930d07f393
Cristopher <109649743+CristopherCabello@users.noreply.github.com>
2023-01-06 23:34:12 -0500

    Add files via upload

---
+++ palindromo.js
@@ -0,0 +1,5 @@
+function palindrome(str){
+    str = str.replace(/[^a-z0-9]/gi,'').toLowerCase()
+    return str===str.split('').reverse().join('')
+}
+console.log(palindrome("A man, a plan, a canal. Panama"))
